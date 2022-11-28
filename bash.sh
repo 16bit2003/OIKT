ff3c7436d0dcdf78d0f4fe7351b3ae6f47d16a70
16bit2003 <87273633+16bit2003@users.noreply.github.com>
2022-11-28 23:10:21 +0100

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 sudo loadkeys si
- 
+wait
+
 mkdir /home/documents
 mkdir /home/downloads
 mkdir /home/pictures
@@ -11,41 +12,48 @@ for i in {1 2 3 4 5}
 do
     mkdir /folder%i
 done
-
+wait
 for i in {1 2 3 4 5}
 do
     sudo adduser User%i
     usermod -aG sudo User%i
 done
-
+wait
 sudo apt-get update
+wait
 sudo apt-get upgrade
+wait
 
 sudo apt-get install nginx
+wait
 sudo apt-get install git
+wait
 sudo apt-get install net-tools
+wait
 sudo apt-get install ufw
+wait
 
 sudo apt-get update
+wait
 
 sudo apt-get install \
     ca-certificates \
     curl \
     gnupg \
     lsb-release
+wait
 
 sudo mkdir -p /etc/apt/keyrings
+wait
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+wait
 
 echo \
 "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
 $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
+wait
 sudo apt-get update
-
+wait
 sudo apt-get install docker-compose-plugin
-
-=======
-echo "hello"
->>>>>>> 96ee0e12d132b81257c2b56e32f306bc33a97157
+wait
