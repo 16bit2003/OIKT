de6ae38571c0b7fdec8c1935010a6e16c60370ac
16bit2003 <87273633+16bit2003@users.noreply.github.com>
2022-11-30 17:14:19 +0100

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -5,9 +5,9 @@ do
     sudo mkdir /folder$i
 done
 wait
-for i in {1..5..1}
+for i in {ena dva tri štiri pet}
 do
-    sudo adduser user$i --force-badname
+    sudo adduser user$i 
     wait
     usermod -aG sudo User$i
 done
