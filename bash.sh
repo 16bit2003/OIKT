02d4531d482b838bfb2007d090c5c3be06caa821
16bit2003 <87273633+16bit2003@users.noreply.github.com>
2022-11-30 17:18:24 +0100

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -11,3 +11,17 @@ do
     wait
     usermod -aG sudo user$i
 done
+wait
+sudo apt-get update
+wait
+sudo apt-get upgrade
+wait
+
+sudo apt-get install nginx
+wait
+sudo apt-get install git
+wait
+sudo apt-get install net-tools
+wait
+sudo apt-get install ufw
+wait
