36d141c2afc39fd62458e7ab4e82e8f433617059
16bit2003 <87273633+16bit2003@users.noreply.github.com>
2022-11-30 17:02:03 +0100

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
-sudo mkdir /documents
-sudo mkdir /downloads
-sudo mkdir /pictures
-sudo mkdir /videos
+for i in {1 2 3 4 5}
+do
+    sudo mkdir /folder$i
+done
+wait
+for i in {1 2 3 4 5}
+do
+    sudo adduser User$i
+    wait
+    usermod -aG sudo User$i
+done
+wait
+sudo apt-get update
+wait
+sudo apt-get upgrade
+wait
